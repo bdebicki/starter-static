97eb7ad072ec7a088f70c56a8083d845cf078daf
Earl Debicki <bartosz.debicki@uxpin.com>
2019-05-26 22:06:21 +0200

    clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 // require gulp
-var gulp = require('gulp');
-var less = require('gulp-less');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCss = require('gulp-clean-css');
-var del = require('del');
+const { dest, pipe, series, src, watch } = require('gulp');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+const del = require('del');
 
-var config = {
+const config = {
 	destFolder: './public/css/',
 	packageFile: 'application.less',
 	srcFolder: './src/less/',
@@ -18,27 +18,27 @@ function clean() {
 
 // build css file task
 function build() {
-	return gulp.src(`${config.srcFolder}${config.packageFile}`)
+	return src(`${config.srcFolder}${config.packageFile}`)
 		.pipe(less())
 		.pipe(autoprefixer())
-		.pipe(gulp.dest(config.destFolder));
+		.pipe(dest(config.destFolder));
 }
 
 // build minified css file task
-function buildClear() {
-	return gulp.src('./src/less/application.less')
+function buildClean() {
+	return src('./src/less/application.less')
 		.pipe(less())
 		.pipe(autoprefixer())
 		.pipe(cleanCss())
-		.pipe(gulp.dest(config.destFolder));
+		.pipe(dest(config.destFolder));
 }
 
 // css watch task
-function watch() {
-	return gulp.watch(`${config.srcFolder}**/*.less`, build);
+function watchFiles() {
+	return watch(`${config.srcFolder}**/*.less`, build);
 }
 
-exports.build = build;
-exports.buildClear = gulp.series(clean, buildClear);
-exports.watch = watch;
 exports.default = build;
+exports.build = build;
+exports.buildClean = series(clean, buildClean);
+exports.watch = watchFiles;
